252f979b1d6b86a8f07c849f3a245a0761a29bbc
Andrej Kyselica <andrej@Kyselica.com>
2017-06-25 14:41:01 -0400

    Added empty state to PureComponent extension references where no state is already passed

--- types/react-virtualized/dist/es/Table.d.ts
+++ types/react-virtualized/dist/es/Table.d.ts
@@ -318,7 +318,7 @@ export const SortIndicator: React.StatelessComponent<{ sortDirection: SortDirect
  * Table component with fixed headers and virtualized rows for improved performance with large data sets.
  * This component expects explicit width, height, and padding parameters.
  */
-export class Table extends PureComponent<TableProps> {
+export class Table extends PureComponent<TableProps, {}> {
     static propTypes: {
         'aria-label': Requireable<string>,
         autoHeight: Requireable<boolean>,
